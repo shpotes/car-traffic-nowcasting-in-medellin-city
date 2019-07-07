9fd22b120c4d50c16c76dfbffcdf33539568c517
Santiago <shinca12@eafit.edu.co>
2019-07-07 14:32:28 -0500

    rlly nigga?

--- model/model.py
+++ model/model.py
@@ -95,6 +95,7 @@ class VGG16(_Model):
             Conv2D(512, (3, 3), activation='relu', padding='same'),
             Conv2D(512, (3, 3), activation='relu', padding='same'),
             MaxPooling2D((2, 2)),
+            Flatten(),
             Dense(4096, activation='relu'),
             Dense(4096, activation='relu'),
             Dense(num_classes, activation='softmax')
